eaea6f1649237af8d0fa2cc725655fdc76708180
Mariano Lopez <lopezmariano3108@gmail.com>
2022-08-05 19:47:34 -0300

    arregle los archivos

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
-//CATALOGO - CONTIENE LOS PRODUCTOS A AGREGAR AL CARRITO
+const DB = "../data.json";
 
-const Catalogo = [
-    { id: 1, nombre: "Leche", img: "./images/leche.jpg", cantidad: 1, descripcion: "una remera...", categoria: "Bebible", precio: 200, stock: true },
-    { id: 2, nombre: "Huevos", img: "./images/huevo.jpg", cantidad: 1, descripcion: "un pantalon...", categoria: "Comestible", precio: 250, stock: true },
-    { id: 4, nombre: "Jamon", img: "./images/jamon.webp", cantidad: 1, descripcion: "un buzo...", categoria: "Comestible", precio: 350, stock: true },
-    { id: 5, nombre: "Queso", img: "./images/queso.webp", cantidad: 1, descripcion: "un buzo...", categoria: "Comestible", precio: 400, stock: true },
-    { id: 6, nombre: "Pan lactal", img: "./images/panlactal.webp", cantidad: 1, descripcion: "un buzo...", categoria: "Comestible", precio: 250, stock: false },
-    { id: 7, nombre: "Harina", img: "./images/harina.webp", cantidad: 1, descripcion: "un buzo...", categoria: "Comestible", precio: 150, stock: true },
-    { id: 8, nombre: "Mayonesa", img: "./images/mayonesa.webp", cantidad: 1, descripcion: "un buzo...", categoria: "Aderezo", precio: 150, stock: true },
-    { id: 9, nombre: "Agua", img: "./images/agua.webp", cantidad: 1, descripcion: "un buzo...", categoria: "Bebible", precio: 100, stock: true },
-    { id: 10, nombre: "Coca Cola", img: "./images/coca.png", cantidad: 1, descripcion: "un buzo...", categoria: "Bebible", precio: 300, stock: true },
-    { id: 11, nombre: "Oreos", img: "./images/oreo.webp", cantidad: 1, descripcion: "un buzo...", categoria: "Galletitas", precio: 300, stock: true },
-    { id: 12, nombre: "Sprite", img: "./images/sprite.webp", cantidad: 1, descripcion: "un buzo...", categoria: "Bebible", precio: 150, stock: false },
-    { id: 13, nombre: "Sonrisas", img: "./images/sonrisas.jpg", cantidad: 1, descripcion: "un buzo...", categoria: "Galletitas", precio: 250, stock: true },
-    { id: 14, nombre: "Rollo de cocina", img: "./images/rollodecocina.webp", cantidad: 1, descripcion: "un buzo...", categoria: "Limpieza", precio: 400, stock: true },
-    { id: 15, nombre: "Jabon liquido", img: "./images/jabonliquido.webp", cantidad: 1, descripcion: "un buzo...", categoria: "Limpieza", precio: 800, stock: true }
-];
+const getDBData = async () => {
+    const res = await fetch(DB)
+    const data = await res.json()
+
+    return data
+}
 
 //CARRITO DE COMPRAS
 
@@ -27,20 +17,25 @@ let cardContainer
 
 const precioDom = document.querySelector("#dom-precio")
 
-for (const iterador of Catalogo) {
-    cardContainer = document.querySelector("#card-container");
-    const cardHTML = `
-        <div class="card-container">
-            <img src=${iterador.img} alt="">
-              <p>${iterador.nombre}</p>
-              <p>${iterador.descripcion}</p>
-              <p>Precio: ${iterador.precio}</p>
-              <button class="btn btn-primary" onClick="agregarAlCarrito(${iterador.id})">Agregar al carrito</button>
-        </div>
-        `
-    iterador.stock && (cardContainer.innerHTML += cardHTML)
-}
+const mostrarCards = async () => {
+    
+    const catalogo = await getDBData()
 
+    for (const iterador of catalogo) {
+        cardContainer = document.querySelector("#card-container");
+        const cardHTML = `
+            <div class="card-container">
+                <img src=${iterador.img} alt="">
+                  <p>${iterador.nombre}</p>
+                  <p>${iterador.descripcion}</p>
+                  <p>Precio: ${iterador.precio}</p>
+                  <button class="btn btn-primary" onClick="agregarAlCarrito(${iterador.id})">Agregar al carrito</button>
+            </div>
+            `
+        iterador.stock && (cardContainer.innerHTML += cardHTML)
+    }
+}
+document.addEventListener("DOMContentLoaded", mostrarCards)
 //FUNCION AGREGAR AL CARRITO SI LA CONDICION "STOCK" SE CUMPLE
 
 function agregarAlCarrito(id) {
