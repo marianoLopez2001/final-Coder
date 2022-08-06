57478d911867eb01da04ee1b70a47063c8f4207a
Mariano Lopez <lopezmariano3108@gmail.com>
2022-08-06 15:48:27 -0300

    asd

--- app.js
+++ app.js
@@ -3,32 +3,30 @@
 import { agregarAlCarrito } from "./agregarCarrito.js";
 import { getDBData } from "./getdata.js";
 
-let cardContainer
+let cardContainer //se puede borrar?
 
 export const mostrarCards = async () => {
 
     const catalogo = await getDBData()
 
     catalogo.forEach(iterador => {
-        cardContainer = document.querySelector("#card-container");
+        const cardContainer = document.querySelector("#card-container");
         const cardHTML = `
             <div class="card-container">
                 <img src=${iterador.img} alt="">
                   <p>${iterador.nombre}</p>
                   <p>${iterador.descripcion}</p>
                   <p>Precio: ${iterador.precio}</p>
-                  <button class="btn btn-primary" id="agregarAlCarrito${iterador.id}")">Agregar al carrito</button>
+                  <button class="btn btn-primary" id=agregarAlCarrito${iterador.id} >Agregar al carrito</button>
             </div>
             `
         iterador.stock && (cardContainer.innerHTML += cardHTML)
 
-        const botonAgregarCarrito = document.getElementById(`agregarAlCarrito${iterador.id}`)
+        let botonAgregarCarrito = document.getElementById(`agregarAlCarrito${iterador.id}`)
 
         console.log(botonAgregarCarrito);
 
-        const log = () => { console.log("asd")}
-
-        botonAgregarCarrito.addEventListener("click", log )
+        botonAgregarCarrito.addEventListener("click", agregarAlCarrito(iterador.id))
     });
 }
 
